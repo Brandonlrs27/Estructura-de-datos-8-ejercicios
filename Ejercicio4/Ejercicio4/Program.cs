7de4cb799e2e0a4fc858c8bb4e4b8e90ca338d51
Brandonlrs27 <43393536+Brandonlrs27@users.noreply.github.com>
2018-09-24 18:02:19 -0700

    4

---
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int menor = 0;
+            Console.WriteLine("Ingrese A, B y C:");
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int C = int.Parse(Console.ReadLine());
+            if (A < B && A < C)
+            {
+                menor = A;
+            }
+            else if (B < A && B < C)
+            {
+                menor = B;
+            }
+            else if (C < B && C < A)
+            {
+                menor = C;
+            }
+            Console.WriteLine("El numero menor entre {0}, {1} y {2} es: {3}",A,B,C,menor);
+            Console.ReadKey();
+        }
+    }
+}
